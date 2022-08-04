c4ec5f0f8b3cce0bd8d35ff64e70bac539092789
Alan Hurtarte <alan.hurtarte@iovlabs.org>
2022-08-03 23:06:26 -0600

    PP-183: Using fixed address and private keys

--- core/lib/wallet_creator/src/main.rs
+++ core/lib/wallet_creator/src/main.rs
@@ -12,10 +12,15 @@ use zksync::{
 
 use zksync_eth_signer::{PrivateKeySigner};
 
-// const ETH_ADDR: &str = "c354d97642faa06781b76ffb6786f72cd7746c97";
-// const ETH_PRIVATE_KEY: &str = "20e4a6381bd3826a14f8da63653d94e7102b38eb5f929c7a94652f41fa7ba323";
-// const LOCALHOST_WEB3_ADDR: &str = "http://127.0.0.1:4444";
-// const DOCKER_WEB3_ADDR: &str = "http://rskj:4444"; 
+const ETH_ADDR: &str = "c354d97642faa06781b76ffb6786f72cd7746c97";
+const ETH_PRIVATE_KEY: &str = "20e4a6381bd3826a14f8da63653d94e7102b38eb5f929c7a94652f41fa7ba323";
+
+fn eth_main_account_credentials() -> (H160, H256) {
+    let addr = ETH_ADDR.parse().unwrap();
+    let eth_private_key = ETH_PRIVATE_KEY.parse().unwrap();
+
+    (addr, eth_private_key)
+}
 
 fn eth_random_account_credentials() -> (H160, H256) {
     let mut eth_private_key = H256::default();
@@ -27,19 +32,18 @@ fn eth_random_account_credentials() -> (H160, H256) {
 }
 
 async fn create_new_wallet() -> Result<Wallet<PrivateKeySigner, RpcProvider>, anyhow::Error>{
-    let (eth_address, eth_private_key) = eth_random_account_credentials();
-    println!("ETH Address: {:?}",eth_address);
-    println!("Private key: {:?}",eth_private_key);
+    let (main_eth_address, main_eth_private_key) = eth_main_account_credentials();
+    // let (main_eth_address, main_eth_private_key) = eth_random_account_credentials();
 
-    let provider = RpcProvider::new(Network::Localhost);
-
-    let eth_signer = PrivateKeySigner::new(eth_private_key);
+    let eth_signer = PrivateKeySigner::new(main_eth_private_key);
     let credentials =
-        WalletCredentials::from_eth_signer(eth_address, eth_signer, Network::Localhost)
+        WalletCredentials::from_eth_signer(main_eth_address, eth_signer, Network::Localhost)
             .await
             .unwrap();
 
+    let provider = RpcProvider::new(Network::Localhost);
     let wallet = Wallet::new(provider, credentials).await?;
+
 
     Ok(wallet)
 }
@@ -48,5 +52,6 @@ async fn create_new_wallet() -> Result<Wallet<PrivateKeySigner, RpcProvider>, an
 async fn main() -> Result<(), anyhow::Error> {
     let _wallet = create_new_wallet().await?;
     // print!("well well well");
+    print!("Wallet created");
     Ok(())
 }
