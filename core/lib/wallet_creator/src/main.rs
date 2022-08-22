1a5212005f846cffab0a736b6eb7a467c35a916b
Alan Hurtarte <alan220193@hotmail.com>
2022-08-22 13:15:38 -0600

    PP-183: fix warning and added tests

--- core/lib/wallet_creator/src/main.rs
+++ core/lib/wallet_creator/src/main.rs
@@ -22,8 +22,8 @@ fn eth_random_account_credentials() -> (H160, H256) {
 }
 
 async fn create_new_wallet(address: &str, private_key: &str) -> Result<Wallet<PrivateKeySigner, RpcProvider>, anyhow::Error>{
-    let mut eth_address:H160;
-    let mut eth_private_key:H256;
+    let eth_address:H160;
+    let eth_private_key:H256;
     if address.is_empty() || private_key.is_empty() {
         (eth_address, eth_private_key) = eth_random_account_credentials();
     } else {
@@ -67,4 +67,48 @@ async fn main() -> Result<(), anyhow::Error> {
     println!("-> Wallet created");
 
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+    // use serde_json as ser;
+
+    #[test]
+    fn test_generate_random_keys() {
+        // assert_eq!(add(1, 2), 3);
+        let eth_address:H160;
+        let eth_private_key:H256;
+        (eth_address, eth_private_key) = eth_random_account_credentials();
+
+        println!("-> Address {:?}", eth_address);
+        println!("-> Private key {:?}", eth_private_key);    
+
+        assert_eq!(eth_address.is_zero(), false);
+        assert_eq!(eth_private_key.is_zero(), false);
+    }
+
+    #[tokio::test]
+    async fn test_using_random_keys() -> Result<(), anyhow::Error>  {
+        let eth_address:H160;
+        let eth_private_key:H256;
+        (eth_address, eth_private_key) = eth_random_account_credentials();
+        let _wallet = create_new_wallet(format!("{:?}",eth_address).as_str(), format!("{:?}",eth_private_key).as_str()).await?;
+        println!("-> Wallet created");
+        assert_eq!(1,1);
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_using_user_address() -> Result<(), anyhow::Error>  {
+        // assert_eq!(bad_add(1, 2), 3);
+        let eth_private_key:H256 = H256::from_low_u64_be(1_000);
+
+        let eth_address = PackedEthSignature::address_from_private_key(&eth_private_key).unwrap();
+        let _wallet = create_new_wallet(format!("{:?}",eth_address).as_str(), format!("{:?}",eth_private_key).as_str()).await?;
+        println!("-> Wallet created");
+        assert_eq!(1,1);
+        Ok(())
+    }
 }
