a1c97379575c9162235b0d0d9b5effa44a19a3dd
Antonio Morrone <antonio@iovlabs.org>
2023-05-30 13:56:21 +0200

    fix(contracts): change the error message returned in ForcedExit

--- contracts/contracts/ForcedExit.sol
+++ contracts/contracts/ForcedExit.sol
@@ -39,7 +39,7 @@ contract ForcedExit is Ownable, ReentrancyGuard {
         uint256 balance = address(this).balance;
 
         (bool success, ) = _to.call{value: balance}("");
-        require(success, "RIF withdraw failed");
+        require(success, "ETH withdraw failed");
     }
 
     // We have to use fallback instead of `receive` since the ethabi
