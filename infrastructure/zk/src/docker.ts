747c165b94f58cb4ab51a6ec72ca9a5cb3c22930
franciscotobar <100875069+franciscotobar@users.noreply.github.com>
2023-06-28 09:05:29 -0600

    ROLLUP-181/Publish Docker Images (#78)
    * build: docker images

--- infrastructure/zk/src/docker.ts
+++ infrastructure/zk/src/docker.ts
@@ -12,16 +12,14 @@ const IMAGES = [
     'ci',
     'exit-tool',
     'dev-liquidity-token-watcher',
-    'zk-environment',
     'event-listener',
     'data-restore'
-    //,`'rskj'
 ];
 
-async function dockerCommand(command: 'push' | 'build', image: string) {
+async function dockerCommand(command: 'push' | 'build', image: string, tag: string) {
     if (image == 'rust') {
-        await dockerCommand(command, 'server');
-        await dockerCommand(command, 'prover');
+        await dockerCommand(command, 'server', tag);
+        await dockerCommand(command, 'prover', tag);
         return;
     }
     if (!IMAGES.includes(image)) {
@@ -31,40 +29,48 @@ async function dockerCommand(command: 'push' | 'build', image: string) {
         image = 'keybase-secret';
     }
     if (command == 'build') {
-        await _build(image);
+        await _build(image, tag);
     } else if (command == 'push') {
-        await _push(image);
+        await _push(image, tag);
     }
 }
 
-async function _build(image: string) {
+async function _build(image: string, tag: string) {
     if (image == 'nginx') {
         await utils.spawn('yarn explorer build');
     }
     if (image == 'server' || image == 'prover') {
         await contract.build();
     }
-    const { stdout: imageTag } = await utils.exec('git rev-parse --short HEAD');
-    const latestImage = `-t matterlabs/${image}:latest`;
-    const taggedImage = ['nginx', 'server', 'prover'].includes(image) ? `-t matterlabs/${image}:${imageTag}` : '';
-    await utils.spawn(`DOCKER_BUILDKIT=1 docker build ${latestImage} ${taggedImage} -f ./docker/${image}/Dockerfile .`);
+
+    const imageTag = `-t rsksmart/rollup-${image}:${tag}`;
+    await utils.spawn(
+        `DOCKER_BUILDKIT=1 docker build ${imageTag} --platform linux/amd64 -f ./docker/${image}/Dockerfile .`
+    );
 }
 
-async function _push(image: string) {
-    await utils.spawn(`docker push matterlabs/${image}:latest`);
-    if (['nginx', 'server', 'prover', 'event-listener'].includes(image)) {
-        const { stdout: imageTag } = await utils.exec('git rev-parse --short HEAD');
-        await utils.spawn(`docker push matterlabs/${image}:${imageTag}`);
-    }
+async function _push(image: string, tag: string) {
+    await utils.spawn(`docker push rsksmart/rollup-${image}:${tag}`);
 }
 
-export async function build(image: string) {
-    await dockerCommand('build', image);
+export async function build(image: string, tag: string) {
+    await dockerCommand('build', image, tag);
 }
 
-export async function push(image: string) {
-    await dockerCommand('build', image);
-    await dockerCommand('push', image);
+export async function buildFromTag(gitTag: string) {
+    const [image_name, image_tag] = gitTag.split(':');
+    await dockerCommand('build', image_name, image_tag);
+}
+
+export async function push(image: string, tag: string) {
+    await dockerCommand('build', image, tag);
+    await dockerCommand('push', image, tag);
+}
+
+export async function pushFromTag(gitTag: string) {
+    const [image_name, image_tag] = gitTag.split(':');
+    await dockerCommand('build', image_name, image_tag);
+    await dockerCommand('push', image_name, image_tag);
 }
 
 export async function restart(container: string) {
@@ -77,7 +83,9 @@ export async function pull() {
 
 export const command = new Command('docker').description('docker management');
 
-command.command('build <image>').description('build docker image').action(build);
-command.command('push <image>').description('build and push docker image').action(push);
+command.command('build <image> <tag>').description('build docker image').action(build);
+command.command('push <image> <tag>').description('build and push docker image').action(push);
+command.command('build-from-tag <gitTag>').description('build docker image from git tag').action(buildFromTag);
+command.command('push-from-tag <gitTag>').description('build and push docker image from git tag').action(pushFromTag);
 command.command('pull').description('pull all containers').action(pull);
 command.command('restart <container>').description('restart container in docker-compose.yml').action(restart);
