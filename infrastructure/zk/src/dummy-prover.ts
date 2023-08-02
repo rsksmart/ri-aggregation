923e2dec3a6d1aa2f09d3a1d0e78efcf2e469d51
Francisco Tobar <francisco.tobar@iovlabs.org>
2023-08-02 09:44:08 -0600

    refactor: pr comments

--- infrastructure/zk/src/dummy-prover.ts
+++ infrastructure/zk/src/dummy-prover.ts
@@ -33,6 +33,10 @@ export async function status() {
 async function setStatus(value: boolean, redeploy: boolean, withDocker: boolean) {
     env.modify('CONTRACTS_TEST_DUMMY_VERIFIER', `CONTRACTS_TEST_DUMMY_VERIFIER="${value}"`);
     env.modify_contracts_toml('CONTRACTS_TEST_DUMMY_VERIFIER', `CONTRACTS_TEST_DUMMY_VERIFIER="${value}"`);
+    if (withDocker) {
+        env.modify('MISC_DOCKER_DUMMY_PROVER', `MISC_DOCKER_DUMMY_PROVER=${value}`);
+        env.modify_contracts_toml('MISC_DOCKER_DUMMY_PROVER', `MISC_DOCKER_DUMMY_PROVER=${value}`);
+    }
     await status();
     if (redeploy) {
         console.log('Redeploying the contract...');
