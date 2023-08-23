565f9032f42f78fdd06b7d80af52108fbf4e1940
Juraj Piar <jura.piar@icloud.com>
2023-08-23 11:39:30 +0100

    feat(simulation): adds test subcommand to zk simulator

---
+++ infrastructure/zk/src/simulator.ts
@@ -0,0 +1,26 @@
+import { Command } from 'commander';
+import * as utils from './utils';
+
+export const simulator = new Command('simulator')
+    .aliases(['s', 'sim', 'simul', 'simulate', 'simulation', 'simulation-tool'])
+    .description('Open up a dedicated TUI for running transaction simulations on Rollup')
+    .action(async (_cmd: Command) => {
+        await utils.spawn('yarn run simulation-tool simulate');
+    });
+
+simulator
+    .command('build')
+    .alias('b')
+    .description('Build the simulator prior to execution')
+    .action(async () => {
+        await utils.spawn('yarn build:simulation-tool');
+    });
+
+simulator
+    .command('test')
+    .alias('t')
+    .description(`Run the simulation tool's unit tests`)
+    .option('-w --watch', 'Watch for changes and re-run tests')
+    .action(async ({ watch }) => {
+        await utils.spawn(`yarn simulation-tool test${watch ? ':watch' : ''} `);
+    });
