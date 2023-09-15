cc656345b5863be5346cbda124cf9ec485e3531b
Antonio Morrone <antonio@iovlabs.org>
2023-09-15 16:28:30 +0200

    test: increate prover timeout

--- core/bin/prover/tests/tests.rs
+++ core/bin/prover/tests/tests.rs
@@ -205,7 +205,7 @@ async fn test_receiving_heartbeats() {
     )
     .fuse();
     // We need to set an higher timeout value to avoid the test to fail
-    let timeout = tokio::time::sleep(Duration::from_secs(30)).fuse();
+    let timeout = tokio::time::sleep(Duration::from_secs(60)).fuse();
 
     pin_mut!(prover_work_cycle, timeout);
 
